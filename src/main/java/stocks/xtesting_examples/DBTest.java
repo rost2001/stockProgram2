dc3087f9861a05236001ce7394175cedb8856cc2
fredrik9 <fj222hn@student.lnu.se>
2021-01-26 16:20:07 +0100

    sql

--- src/main/java/stocks/xtesting_examples/DBTest.java
+++ src/main/java/stocks/xtesting_examples/DBTest.java
@@ -2,26 +2,28 @@ package stocks.xtesting_examples;
 
 //STEP 1. Import required packages
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DBTest {
 
     // JDBC driver name and database URL
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://localhost/EMP";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/stockdata2";
 
     // Database credentials
-    static final String USER = "username";
-    static final String PASS = "password";
+    static final String USER = "root";
+    static final String PASS = "Uctrickme12";
+
+    static Connection conn = null;
+    static Statement stmt = null;
 
     public static void main(String[] args) {
-	Connection conn = null;
-	Statement stmt = null;
+
 	try {
-	    
-	    
-	    
+
+
 	    // STEP 2: Register JDBC driver
-	    Class.forName("com.mysql.jdbc.Driver");
+	    Class.forName("com.mysql.cj.jdbc.Driver");
 
 	    // STEP 3: Open a connection
 	    System.out.println("Connecting to database...");
@@ -31,53 +33,85 @@ public class DBTest {
 	    System.out.println("Creating statement...");
 	    stmt = conn.createStatement();
 	    String sql;
-	    sql = "SELECT id, first, last, age FROM Employees";
-	    ResultSet rs = stmt.executeQuery(sql);
 
-	    // STEP 5: Extract data from result set
-	    while (rs.next()) {
-		// Retrieve by column name
-		int id = rs.getInt("id");
-		int age = rs.getInt("age");
-		String first = rs.getString("first");
-		String last = rs.getString("last");
+	    //----------------------------------------------
 
-		// Display values
-		System.out.print("ID: " + id);
-		System.out.print(", Age: " + age);
-		System.out.print(", First: " + first);
-		System.out.println(", Last: " + last);
+
+
+	    sql = "SELECT * FROM stocks;";
+
+
+
+
+	    ArrayList<Object[]> data = getData(sql);
+
+
+
+	    String sql2 = "";
+	    sql2 = "INSERT INTO stocks (symbol, price)\r\n" 
+		    + "VALUES ('AAPL', 131);";
+
+
+	    stmt.executeUpdate(sql2);
+
+
+	    data = getData(sql);
+
+	    // Display values
+	    for(Object[] obj : data) {
+		System.out.println("-------------------");
+		System.out.println("symbol: " + (String)obj[0]);
+		System.out.println("Price: " + (double)obj[1]);
 	    }
+
+
+	    //-----------------------------------------------------
+
 	    // STEP 6: Clean-up environment
-	    rs.close();
+
 	    stmt.close();
 	    conn.close();
-	    
-	    
-	    
-	    
-	    
-	    
-	} catch (SQLException se) {
-	    // Handle errors for JDBC
-	    se.printStackTrace();
+
 	} catch (Exception e) {
-	    // Handle errors for Class.forName
+
 	    e.printStackTrace();
 	} finally {
 	    // finally block used to close resources
 	    try {
 		if (stmt != null)
 		    stmt.close();
-	    } catch (SQLException se2) {
-	    } // nothing we can do
-	    try {
+
 		if (conn != null)
 		    conn.close();
 	    } catch (SQLException se) {
 		se.printStackTrace();
-	    } // end finally try
-	} // end try
+	    } 
+	} 
+
+
+
+
 	System.out.println("Goodbye!");
-    }// end main
-}// end FirstExample
+    }
+
+
+
+    public static ArrayList<Object[]> getData(String sql) throws SQLException {
+	ArrayList<Object[]> data = new ArrayList<Object[]>();
+
+	ResultSet rs = stmt.executeQuery(sql);
+
+	while (rs.next()) {
+	    // Retrieve by column name
+
+	    Object[] temp = new Object[2];
+	    temp[0] = rs.getString("symbol");
+	    temp[1] = rs.getDouble("price");
+
+	    data.add(temp);
+	}
+	rs.close();
+	return data;
+    }
+
+}
